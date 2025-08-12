ceb497cbb305d714b2cf73a6f3e3308cf8ef7f34
roman alekseev <roma.alekseev.90@bk.ru>
2025-08-12 10:27:31 +0300

    Добавлен компонент Hero

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -1,16 +1,20 @@
 "use client";
-import { useDispatch, useSelector } from "react-redux";
+
+import { useSelector } from "react-redux";
 import styles from "./Burger.module.sass";
 import { RootState } from "@/store/store";
-import { toggleMenu } from "@/store/slice/menuSlice";
 
-const Burger = () => {
-  const dispatch = useDispatch();
+interface BurgerProps {
+  onClick: () => void;
+}
+
+const Burger = ({ onClick }: BurgerProps) => {
   const isOpen = useSelector((state: RootState) => state.menu.isOpen);
 
   const handleClick = () => {
-    dispatch(toggleMenu());
+    onClick();
   };
+
   return (
     <button
       className={`${styles.burger} ${isOpen ? styles.active : ""}`}
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,27 @@
+import styles from "./Hero.module.sass";
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+      <div className={styles.container}>
+        <div className={styles.desc}>
+          <h2 className={styles.title}>ROBO SCHOOL</h2>
+          <p className={styles.text}>
+            Курсы повышения квалификации по робототехнике для педагогов
+            начальной школы
+          </p>
+          <button className={styles.btn}>Записаться на курс</button>
+        </div>
+        <Image
+          src={"/image/hero-img.png"}
+          width={783}
+          height={918}
+          alt="Hero Image"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
