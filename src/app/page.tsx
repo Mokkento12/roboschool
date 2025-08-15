4555de678f8fc20af3ba088bf38e8a0f4bf7c6d3
roman alekseev <roma.alekseev.90@bk.ru>
2025-08-15 11:19:22 +0300

    Создан компонент Benefits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/About/About";
+import Benefits from "@/components/Benefits/Benefits";
 import Header from "@/components/Header/Header";
 import Hero from "@/components/Hero/Hero";
 
@@ -8,6 +9,7 @@ export default function HomePage() {
       <Header />
       <Hero />
       <About />
+      <Benefits />
     </>
   );
 }
---
+++ src/components/Benefits/Benefits.tsx
@@ -0,0 +1,11 @@
+import styles from "./Benefits.module.sass";
+
+const Benefits = () => {
+  return (
+    <div className={styles.benefits}>
+      <div className={styles.container}>BENEFITS</div>
+    </div>
+  );
+};
+
+export default Benefits;
