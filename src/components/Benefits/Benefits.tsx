e0c5b11ce9b1e1a1a349978d514d76c2ef25b9e4
roman alekseev <roma.alekseev.90@bk.ru>
2025-08-16 12:14:32 +0300

    Доделан компонент Benefits

--- src/components/Benefits/Benefits.tsx
+++ src/components/Benefits/Benefits.tsx
@@ -1,9 +1,31 @@
+import { benefits } from "../../data/benefits";
 import styles from "./Benefits.module.sass";
 
 const Benefits = () => {
   return (
     <div className={styles.benefits}>
-      <div className={styles.container}>BENEFITS</div>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Что вы получите после курса</h2>
+        <div className={styles.items}>
+          {benefits.map((item, index) => (
+            <div key={index} className={styles.benefit}>
+              {/* Оборачиваем заголовок и кнопку в один блок */}
+              <div className={styles.benefitHeader}>
+                <h2 className={styles.benefitTitle}>{item.title}</h2>
+                {index === benefits.length - 1 && (
+                  <button className={styles.infoButton}>
+                    <span className={styles.infoIcon}>i</span>
+                    <span className={styles.tooltip}>
+                      При наличии свободных мест
+                    </span>
+                  </button>
+                )}
+              </div>
+              <p className={styles.text}>{item.text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
