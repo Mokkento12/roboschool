4b904223b25a9d12912689caf24842a10294526a
roman alekseev <roma.alekseev.90@bk.ru>
2025-09-01 11:42:26 +0300

    Закончил вертску компонента формы

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,7 +1,44 @@
 import styles from "./Form.module.sass";
 
 const Form = () => {
-  return <div className={styles.form}>FORM</div>;
+  return (
+    <div className={styles.formWrapper}>
+      <div className={styles.container}>
+        <div className={styles.desc}>
+          <h2 className={styles.title}>
+            Запишитесь на курс <br />
+            со скидкой 10%
+          </h2>
+          <p className={styles.text}>
+            Акция действительна до 10 марта 2022 года
+          </p>
+        </div>
+        <form className={styles.form}>
+          <input
+            type="text"
+            placeholder="Имя"
+            required
+            className={styles.input}
+          />
+          <input
+            type="phone"
+            placeholder="Телефон"
+            required
+            className={styles.input}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            className={styles.input}
+          />
+          <button type="submit" className={styles.submitBtn}>
+            Отправить заявку
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Form;
