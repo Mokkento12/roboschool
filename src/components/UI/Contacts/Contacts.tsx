23c9c0d21219ed43f4c246d61af956f8127bbaf0
roman alekseev <roma.alekseev.90@bk.ru>
2025-09-05 12:22:36 +0300

    Мобильные стили для футера

--- src/components/UI/Contacts/Contacts.tsx
+++ src/components/UI/Contacts/Contacts.tsx
@@ -1,9 +1,21 @@
+// components/UI/Contacts/Contacts.tsx
+
 import Link from "next/link";
 import styles from "./Contacts.module.sass";
-const Contacts = () => {
+
+interface ContactsProps {
+  variant?: "header" | "footer"; // ✅ Добавлен пропс variant
+}
+
+const Contacts = ({ variant = "header" }: ContactsProps) => {
   return (
     <div>
-      <Link href="tel:+7 800 000 11 22" className={styles.tel}>
+      <Link
+        href="tel:+7 800 000 11 22"
+        className={`${styles.tel} ${
+          variant === "footer" ? styles.footerTel : ""
+        }`}
+      >
         +7 800 000 11 22
       </Link>
     </div>
